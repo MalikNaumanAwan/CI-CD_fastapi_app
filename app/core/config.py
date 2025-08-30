fceca9d9b74e5fa682281785764d4b0ab7d8263b
MalikNaumanAwan <maliknauman0@gmail.com>
2025-08-30 06:18:11 -0700

    new updated

--- app/core/config.py
+++ app/core/config.py
@@ -6,8 +6,7 @@ class Settings(BaseSettings):
     APP_NAME: str = "fastapi-ci-demo"
     API_PREFIX: str = "/api"
     DATABASE_URL: AnyUrl = (
-        "postgresql://neondb_owner:npg_z3pGAn5WPvKw@ep-winter-lab-ady4yz5q-pooler.c-2.us-east-1.aws.neon.tech/demo_ci?sslmode=require&channel_binding=require"
-        # "postgresql+asyncpg://postgres:q123@localhost:5432/ci_demo"  # "postgresql+asyncpg://devuser:devpass@db:5432/demo_ci"
+        "postgresql+asyncpg://postgres:q123@localhost:5432/ci_demo"  # "postgresql+asyncpg://devuser:devpass@db:5432/demo_ci"
     )
     CORS_ORIGINS: list[str] = ["*"]
 
